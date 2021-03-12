372ba17305319a18e5198a6355d9fbe896f6c166
Mint Project <minttran.9002@gmail.com>
2021-03-12 19:32:50 +0700

    Change redirect Uri

--- src/api/credential.js
+++ src/api/credential.js
@@ -3,7 +3,7 @@ const Credential = () => {
     authEndPoint : "https://accounts.spotify.com/authorize",
     ClientId: "e4cb4b85fb0c4decbb27bce0b1493ab3",
     ClientSecret: "92d21f0556e04edfb70bfff80c628ad3",
-    RedirectUri : 'http://localhost:3000/',
+    RedirectUri : 'https://spotify-clone-app-414f0.web.app/',
     Scopes : [
       "user-read-currently-playing",
       "user-read-recently-played",
